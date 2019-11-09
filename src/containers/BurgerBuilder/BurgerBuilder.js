7daa69c5b4aab5f66c5999b02d4e1144a8c85926
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-11-09 21:59:14 +0100

    Added state to BurgerBuilder and setted state in Burger.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,24 @@ import Aux from '../../hoc/AuxComponent';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+    //ALTERNATYWA DLA STATE:
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
             <Aux>
                 {/*tu będzie wizualizacja Burgera*/}
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 {/*//tu będzie panel zarządzający usuwaniem i dodawaniem skłądników*/}
                 <div>Build Controls</div>
             </Aux>
