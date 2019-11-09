1f241b610b741ac3ffdf9be86277b6d8c66cd22d
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-11-09 13:21:22 +0100

    Imported Burger in BurgerBuilder and added component in it as JSX.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/AuxComponent';
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
     render() {
         return (
             <Aux>
                 {/*tu będzie wizualizacja Burgera*/}
-                <div>Burger</div>
+                <Burger/>
                 {/*//tu będzie panel zarządzający usuwaniem i dodawaniem skłądników*/}
                 <div>Build Controls</div>
             </Aux>
