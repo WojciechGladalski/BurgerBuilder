cb5dd51d2d4c9c9ac030aeb0dc6267a78cd93671
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-11-10 08:52:15 +0100

    Added reducing an array of ingredients and added condition if there is no ingredients, show info.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
-        });
+        }).reduce((array, element) => {
+            return (array.concat(element))
+        }, []);
+    //Powyżej tworzę tablicę składników z wewnętrznymi tablicami każdego składnika (może byc ich więcej niż jeden) a potem redukuję,
+    //żeby powstała jedns tablica składników. Poniżej: jeżeli nie ma składników, wtedy wyświetl informację.
+    console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
