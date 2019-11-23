4bfc3da4c872389c914c50cc720dd977ce13d05d
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-11-23 19:12:22 +0100

    Modified logo params to adjust it to mobile devices.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css';
+import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
 
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <Logo height="11%"/>
             <nav>
                 <NavigationItems/>
             </nav>
