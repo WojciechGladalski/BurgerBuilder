cfb7dd6cc072234c9b6416e4b35eeebf1202e64f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-11-24 21:03:22 +0100

    Made MENU wrapping after clicked on it.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,17 +8,27 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+        //W taki sposób (poprzez this.state) lepiej tego nie robić, lepszy spopsób powyżej.
+        // this.setState({showSideDrawer: !this.state.showSideDrawer});
+    }
+
     render() {
         return (
             <Aux>
-                <Toolbar/>
+                {/*Jesli w {} jest this.jakaśMetoda to gdyby była z nawiasami na końcu była by wykonywana (executed) ale */}
+                {/*zawsze należy używać REFERENCJI do metody w klasie*/}
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}/>
