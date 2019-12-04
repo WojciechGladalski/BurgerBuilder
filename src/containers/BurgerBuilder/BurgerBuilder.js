277fc586986ba4ab935f3a78b6cd3dcfbe8f9c35
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-04 20:32:49 +0100

    Added proper content to purchaseContinueHandler, sent order data to the server.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
     salad: 0.4,
@@ -88,7 +89,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        //alert("You continue!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Robert Sawyer',
+                address: {
+                    street: 'Jagiellonska 13',
+                    zipCode: '97-500',
+                    country: 'Poland'
+                },
+                email: 'sawyer@wp.pl'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
