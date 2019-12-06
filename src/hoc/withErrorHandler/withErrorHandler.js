2b05bd9b08388807bf42f419cc6c41aebac0f5ef
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-06 16:14:42 +0100

    Added withErrorHandler component.

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../AuxComponent/AuxComponent';
+import Modal from '../../components/UI/Modal/Modal';
+const withErrorHandler = (WrappedComponent) => {
+    return (props) => {
+        return(
+            <Aux>
+                
+            </Aux>
+        )
+    }
+};
+
+export default withErrorHandler;
