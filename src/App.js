30ec5a830fca4e1cc67ee9459215782d112c454f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-21 15:49:28 +0100

    Added Checkout component.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+//wrzucamy Checkout na chwilę, by zobaczyć jak wygląda
+import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder/>
+          <Checkout/>
         </Layout>
       </div>
     );
